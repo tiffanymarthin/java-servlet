33284701a8a1e9ed1b46cd37dcbef970e20d2f04
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-16 00:01:26 -0700

    Implemented doPost method

--- src/main/java/servlet/TextAnalysisServlet.java
+++ src/main/java/servlet/TextAnalysisServlet.java
@@ -1,5 +1,7 @@
 package servlet;
 
+import com.google.gson.JsonObject;
+import java.io.PrintWriter;
 import java.net.URL;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -10,10 +12,11 @@ import org.apache.logging.log4j.Logger;
 
 @WebServlet(name = "TextAnalysisServlet", urlPatterns = "/textbody/*")
 public class TextAnalysisServlet extends HttpServlet {
-    private static final Logger logger = LogManager.getLogger(TextAnalysisServlet.class.getName());
-    private static final String URL_PARAM = "textbody";
 
-    @Override
+  private static final Logger logger = LogManager.getLogger(TextAnalysisServlet.class.getName());
+  private static final String URL_PARAM = "textbody";
+
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
@@ -22,11 +25,34 @@ public class TextAnalysisServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    String urlPath = request.getPathInfo();
+
+    PrintWriter out = response.getWriter();
+    // check if we have a URL!
+    if (urlPath == null || urlPath.isEmpty()) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      out.write("Missing parameters.");
+      return;
+    }
+
+    String[] urlArr = urlPath.split("/");
+    if (!isUrlValid(urlArr)) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      out.write("Parameters are not valid.");
+    } else {
+      response.setStatus(HttpServletResponse.SC_OK);
+      JsonObject jsonResp = new JsonObject();
+      jsonResp.addProperty("message", 0);
+      out.write(String.valueOf(jsonResp));
+      out.flush();
+    }
 
   }
 
   private boolean isUrlValid(String[] urlPath) {
-        int n = urlPath.length;
-        return n == 3 && urlPath[1].equals(URL_PARAM);
+    int n = urlPath.length;
+    return n == 3 && urlPath[1].equals(URL_PARAM);
   }
 }
