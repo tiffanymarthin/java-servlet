93bae2548a779a63d17045ef35584a7729116d61
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-15 23:50:01 -0700

    implement if url valid or not check

--- src/main/java/servlet/TextAnalysisServlet.java
+++ src/main/java/servlet/TextAnalysisServlet.java
@@ -1,5 +1,6 @@
 package servlet;
 
+import java.net.URL;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -10,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 @WebServlet(name = "TextAnalysisServlet", urlPatterns = "/textbody/*")
 public class TextAnalysisServlet extends HttpServlet {
     private static final Logger logger = LogManager.getLogger(TextAnalysisServlet.class.getName());
+    private static final String URL_PARAM = "textbody";
 
     @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -21,5 +23,10 @@ public class TextAnalysisServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
+  }
+
+  private boolean isUrlValid(String[] urlPath) {
+        int n = urlPath.length;
+        return n == 3 && urlPath[1].equals(URL_PARAM);
   }
 }
